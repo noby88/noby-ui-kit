2f146e7d938b9397b0fba29b89f52e684acb276c
Zoltan Szabo <ZoltanZ.Szabo@endava.com>
2021-08-31 16:59:08 +0300

    types present for package in npm

---
+++ dist/Button/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+interface IProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+}
+declare const Button: (props: IProps) => JSX.Element;
+export default Button;
